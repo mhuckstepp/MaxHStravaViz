68c1f360d9abe0aa10a94cc44e5a1b22f2801796
Max Huckstepp <mhuckstepp@gmail.com>
2021-08-24 16:51:59 -0700

    working on strava integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
-import './App.css';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './components/Profile';
-import { useAuth0 } from "@auth0/auth0-react"
-import Card from 'react-bootstrap/Card'
-import RunningStats from './components/RunningStats';
-import { Route } from 'react-router-dom'
-
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import Card from "react-bootstrap/Card";
+import RunningStats from "./components/RunningStats";
+import { Route } from "react-router-dom";
 
 function App() {
-  const { isLoading } = useAuth0()
-  
-  if (isLoading) return <div> Loading...</div>
-  
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div> Loading...</div>;
+
   return (
     <div className="App">
-      <LoginButton></LoginButton>
-      <LogoutButton></LogoutButton>
-      <Card className='userCard'>
-      <Profile></Profile>
-      <Route path='/stats'>
+      <Route exact path="/">
+        <LoginButton></LoginButton>
+        <LogoutButton></LogoutButton>
+        <Card className="userCard">
+          <Profile></Profile>
+        </Card>
+        Here
+        <a
+          href={
+            "https://www.strava.com/oauth/authorize?client_id=69294&redirect_uri=https://www.maxrunmax.xyz/stats&response_type=code&scope=read"
+          }
+        >
+          Login to Strava
+        </a>
+      </Route>
+      <Route path="/stats">
         <RunningStats />
       </Route>
-      </Card>
     </div>
   );
 }
--- src/components/RunningStats.tsx
+++ src/components/RunningStats.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 const RunningStats = () => {
+    const [stravaInfo, setStravaInfo] = useState('')
+    
+    
+    useEffect(() => {
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const params = Object.fromEntries(urlSearchParams.entries())
+        console.log(params);
+    }, [])
+
+
+    // useEffect(() => {
+    //     axios.post('https://www.strava.com/oauth/token')
+        
+    // }, [])
+
+    if (!stravaInfo){
+        return <div>Loading...</div>
+    }
+    
     return (
         <div>
-            hello from stats
+            {JSON.stringify(stravaInfo)}
         </div>
     )
 }
