d7383b40a779ab7141f23bb7be3e7a530611b0bc
Max Huckstepp <mhuckstepp@gmail.com>
2021-08-24 14:16:03 -0700

    change to redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
 import { useAuth0 } from "@auth0/auth0-react"
-import Card from 'react-bootstrap/card'
+import Card from 'react-bootstrap/Card'
 
 
 function App() {
